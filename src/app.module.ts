0341b2e5978803b361e8e4e356f46554117b4740
Phantola <phantoladev@gmail.com>
2023-10-18 13:35:06 +0900

    Version Up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardsModule } from './boards/boards.module';
-import { typeORMConfig } from './configs/typeorm.config';
-import { AuthModule } from './auth/auth.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { MongooseModule } from '@nestjs/mongoose';
-import * as config from 'config';
-
-const mongo = config.get('mongo');
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-	imports: [
-		TypeOrmModule.forRoot(typeORMConfig),
-		BoardsModule,
-		AuthModule,
-		GraphQLModule.forRoot<ApolloDriverConfig>({
-			driver: ApolloDriver,
-			debug: false,
-			playground: true,
-			autoSchemaFile: true,
-		}),
-		MongooseModule.forRoot(mongo.uri),
-	],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
