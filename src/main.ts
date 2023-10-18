a0f7070b303c6f4008bd33fe50e7f9fa19712f25
Phantola <phantoladev@gmail.com>
2023-10-18 16:11:55 +0900

    Add : @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => {
+    Logger.log(`Server Listening on port ${process.env.PORT}`)
+  });
 }
 bootstrap();
