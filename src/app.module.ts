a8aec7633842ce19513447f9ab32499d53a5748c
Phantola <phantoladev@gmail.com>
2023-10-19 15:28:23 +0900

    Fix AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [UserModule, ConfigModule.forRoot(), PostModule],
+  imports: [ConfigModule.forRoot(), UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
